c204e0b8c9350edc10b4a56da613a2776dd7b047
Jhenie <highhalma@gmail.com>
2026-05-05 11:12:28 -0300

    more tests and functions added

--- 01 - Calculator/calc.py
+++ 01 - Calculator/calc.py
@@ -9,3 +9,18 @@ def mult(x, y):
 
 def div(x, y):
     return x / y
+
+def min(x, y):
+    if (x < y):
+        return x
+    else:
+        return y
+    
+def odd(x):
+    return x % 2 == 0
+
+def celtofah(c):
+    return (c * 1.8) + 32
+    
+def fahtocel(f):
+    return (f - 32) * 5/9
--- 01 - Calculator/test_calc.py
+++ 01 - Calculator/test_calc.py
@@ -1,4 +1,4 @@
-from calc import soma, sub, mult, div
+from calc import soma, sub, mult, div, min, odd, celtofah, fahtocel
 
 def test_somar():
     assert soma(3, 2) == 5
@@ -12,3 +12,17 @@ def test_multiplicar():
 def test_dividir():
     assert div(20, 4) == 5
     assert div(10, 0) == "erro"
+    
+def test_min():
+    assert min(10, 2) == 2
+    assert min(5, 12) == 5
+    
+def test_odd():
+    assert odd(2) == True
+    assert odd(3) == False
+    
+def test_celtofah():
+    assert celtofah(50) == 122
+    
+def test_fahtocel():
+    assert fahtocel(50) == 10
